80cff179198970ef93f3988fa98ed927eea1f9d6
binarybruin <michelle.bruin@gmail.com>
2014-10-31 11:00:23 -0400

    add attributes to devices and make Home/default view the Sign Out page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,9 +10,9 @@
 angular.module('sesameDevicesApp')
   .controller('MainCtrl', function ($scope) {
     $scope.devices = [
-      {'name': 'Nexus 5', 'currStatus': 'in'},
-      {'name': 'iPhone 4s', 'currStatus': 'out'},
-      {'name': 'Samsung Galaxy S4', 'currStatus': 'out'}
+      {'name': 'Nexus 5', 'platform': 'Android', 'os': 'KitKat', 'currStatus': 'in'},
+      {'name': 'iPhone 4s', 'platform': 'iOS', 'os': '5.1', 'currStatus': 'out'},
+      {'name': 'Samsung Galaxy S4', 'platform': 'Android', 'os': 'KitKat', 'currStatus': 'out'}
     ];
 
     $scope.orderProp = 'name';
